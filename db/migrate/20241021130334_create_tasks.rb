69114f5d69067cc2abfc49cbabc1e33532f64bda
Oki Hana <hana@hananoMacBook-Air.local>
2024-10-21 23:40:59 +0900

    add/tasksコントローラーの作成とtask周りの機能の実装

---
+++ db/migrate/20241021130334_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.integer :user_id
+      t.string :content
+      t.datetime :deadline
+      t.boolean :priority
+      t.boolean :status
+      t.string :image_id
+
+      t.timestamps
+    end
+  end
+end
