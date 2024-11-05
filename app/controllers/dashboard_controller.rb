e8efb2466d548535e0c54557baa4387a2fb4fda0
Oki Hana <hana@hananoAir.lan>
2024-11-05 23:00:08 +0900

    add/ボタン・リンク等の実装

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class DashboardController < ApplicationController
   def show
-    @upcoming_tasks = current_user.tasks.where(completed: false).where('deadline >= ?', Time.zone.today)
+    @upcoming_tasks = current_user.tasks
+    .where(completed: false)
+    .where('deadline >= ?', Time.zone.today)
+    .order(priority: :desc, deadline: :asc)
     @completed_tasks_today = current_user.tasks.where(completed: true, completed_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def format_datetime(datetime)
+    datetime.strftime("%Y年%m月%d日 %H:%M")
+  end
 end
+  
