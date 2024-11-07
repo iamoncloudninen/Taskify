add03551c3ad7263d78293e667c397b58ef184d7
Oki Hana <hana@hananoMacBook-Air.local>
2024-11-07 20:48:09 +0900

    add/期限を過ぎたタスク表示の実装

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,13 @@
 class DashboardController < ApplicationController
   def show
+    current_time = Time.zone.now
     @upcoming_tasks = current_user.tasks
     .where(completed: false)
-    .where('deadline >= ?', Time.zone.today)
+    .where('deadline >= ?', current_time)
     .order(priority: :desc, deadline: :asc)
-    @completed_tasks_today = current_user.tasks.where(completed: true, completed_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+    @outdated_tasks = current_user.tasks
+    .where(completed: false)
+    .where('deadline < ?', current_time)
+    @completed_tasks_today = current_user.tasks.where(completed: true, completed_at: current_time.beginning_of_day..current_time.end_of_day)
   end
 end
