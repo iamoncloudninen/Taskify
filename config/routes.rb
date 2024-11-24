12d6c9d8945b2cec7bf16cd22f212e37ea3461ed
Oki Hana <hana@hananoMacBook-Air.local>
2024-11-24 21:59:01 +0900

    add/ポモドーロタイマーの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'pomodoro/index'
   get 'dashboard/show'
   get 'timeline/index'
+  get 'timers', to: 'timers#index'
   resources :tasks do
     patch :complete, on: :member
     patch :incomplete, on: :member
---
+++ test/controllers/pomodoro_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class PomodoroControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get pomodoro_index_url
+    assert_response :success
+  end
+end
