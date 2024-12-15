d95ebe5ee05734a66e829b99df423f49aeb255f3
Oki Hana <hana@hananoMacBook-Air.local>
2024-12-15 23:07:37 +0900

    add/タイムラインのデザイン作成

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -2,21 +2,9 @@ class ReactionsController < ApplicationController
   before_action :find_post
 
   def create
-    unless @post.reactions.exists?(user: current_user)
-      @post.reactions.create(user: current_user)
-      flash[:notice] = "頑張ったね！"
-    else
-      flash[:alert] = "すでに頑張ったね！を送っています。"
-    end
-    redirect_to timeline_index_path
-  end
-
-  def destroy
-    reaction = @post.reactions.find_by(user: current_user)
-    if reaction
-      reaction.destroy
-      flash[:notice] = "頑張ったね！を取り消しました。"
-    end
+    @timeline_posts = TimelinePost.includes(reactions: :user).all
+    @post.reactions.create(user: current_user)
+    flash[:notice] = "頑張ったね！"
     redirect_to timeline_index_path
   end
 
