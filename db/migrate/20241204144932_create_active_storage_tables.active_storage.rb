a8ab570fc750dac99ab6a7f7ef1c0ab68418fab8
Oki Hana <hana@hananoMacBook-Air.local>
2025-01-05 21:25:44 +0900

    fix/マイグレーションファイルの修正

--- db/migrate/20241204144932_create_active_storage_tables.active_storage.rb
+++ db/migrate/20241204144932_create_active_storage_tables.active_storage.rb
@@ -2,7 +2,7 @@
 
 class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
   def change
-    primary_key_type, foreign_key_type = primary_and_foreign_key_types
+    primary_key_type, foreign_key_type = determine_key_types
 
     create_active_storage_blobs_table(primary_key_type)
     create_active_storage_attachments(foreign_key_type)
@@ -22,45 +22,32 @@ class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
       t.string   :checksum,     null: false
       t.datetime :created_at,   null: false
     end
-  end
-
-  def add_index_for_blobs
     add_index :active_storage_blobs, [:key], unique: true
   end
 
   def create_active_storage_attachments(foreign_key_type)
     create_table :active_storage_attachments, id: primary_key_type do |t|
       t.string     :name,     null: false
-      t.references :record,   null: false, polymorphic: true, index: false, type: foreign_key_type
+      t.references :record,   null: false, polymorphic: true, type: foreign_key_type
       t.references :blob,     null: false, type: foreign_key_type
-      t.datetime :created_at, null: false
+      t.datetime   :created_at, null: false
     end
-    add_index_for_attachments
-  end
-
-  def add_index_for_attachments
     add_index :active_storage_attachments, %i[record_type record_id name blob_id],
               name: 'index_active_storage_attachments_uniqueness', unique: true
   end
 
   def create_active_storage_variant_records(foreign_key_type)
     create_table :active_storage_variant_records, id: primary_key_type do |t|
-      t.belongs_to :blob, null: false, index: false, type: foreign_key_type
+      t.belongs_to :blob, null: false, type: foreign_key_type
       t.string :variation_digest, null: false
     end
-    add_index_for_variant_records
-  end
-
-  def add_index_for_variant_records
     add_index :active_storage_variant_records, %i[blob_id variation_digest],
               name: 'index_active_storage_variant_records_uniqueness', unique: true
   end
 
-  def primary_and_foreign_key_types
-    config = Rails.configuration.generators
-    setting = config.options[config.orm][:primary_key_type]
-    primary_key_type = setting || :primary_key
-    foreign_key_type = setting || :bigint
+  def determine_key_types
+    primary_key_type = Rails.configuration.generators.options.dig(:active_record, :primary_key_type) || :bigint
+    foreign_key_type = primary_key_type
     [primary_key_type, foreign_key_type]
   end
 end
