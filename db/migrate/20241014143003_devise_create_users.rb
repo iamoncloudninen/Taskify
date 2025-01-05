e7376929ad1cc1620d5fc6e91d47c5d14b874a9b
Oki Hana <hana@hananoMacBook-Air.local>
2025-01-05 22:00:03 +0900

    fix/usersテーブルの修正

--- db/migrate/20241014143003_devise_create_users.rb
+++ db/migrate/20241014143003_devise_create_users.rb
@@ -2,27 +2,18 @@
 
 class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
-    create_users_table
-    add_user_indexes
-  end
-
-  private
-
-  def create_users_table
     create_table :users do |t|
-      t.string :email,              null: false, default: ''
+      t.string :email, null: false, default: ''
       t.string :encrypted_password, null: false, default: ''
-      t.string   :reset_password_token
+      t.string :reset_password_token
       t.datetime :reset_password_sent_at
       t.datetime :remember_created_at
       t.string :username
       t.string :profile_image_id
       t.timestamps null: false
     end
-  end
 
-  def add_user_indexes
-    add_index :users, :email,                unique: true
+    add_index :users, :email, unique: true
     add_index :users, :reset_password_token, unique: true
   end
 end
