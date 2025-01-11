a6b7cf954f92259b2e44ab12d2ab55fdd122b616
Oki Hana <hana@hananoMacBook-Air.local>
2025-01-11 19:11:14 +0900

    fix/AWS設定の修正

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,21 +1,20 @@
 require 'aws-sdk-s3'
 
 if Rails.env.production?
-  s3_client = Aws::S3::Resource.new(
-    region: ENV['AWS_REGION'],
-    credentials: Aws::Credentials.new(
-      ENV['AWS_ACCESS_KEY_ID'],
-      ENV['AWS_SECRET_ACCESS_KEY']
+    Refile.cache = Refile::S3.new(
+      bucket: ENV['AWS_BUCKET'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_REGION'],
+      prefix: 'cache'
     )
-  )
-
-  Refile.cache = Refile::S3.new(
-    bucket: ENV['AWS_BUCKET'],
-    client: s3_client
-  )
   
-  Refile.store = Refile::S3.new(
-    bucket: ENV['AWS_BUCKET'],
-    client: s3_client
-  )
-end
+    Refile.store = Refile::S3.new(
+      bucket: ENV['AWS_BUCKET'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_REGION'],
+      prefix: 'store'
+    )
+  end
+  
