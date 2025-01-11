ba4f842f4c3b5c258befe06b7e52d194eb028f2a
Oki Hana <hana@hananoMacBook-Air.local>
2025-01-11 19:34:37 +0900

    fix/refile.rbの修正

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,20 +1,13 @@
-require 'aws-sdk-s3'
+require "refile/backend/s3"
+require "aws-sdk-s3"
 
-  if Rails.env.production?
-    Refile.cache = Refile::Backend::S3.new(
-      bucket: ENV['AWS_BUCKET'],
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_REGION'],
-      prefix: 'cache'
-    )
-  
-    Refile.store = Refile::Backend::S3.new(
-      bucket: ENV['AWS_BUCKET'],
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_REGION'],
-      prefix: 'store'
-    )
-  end
-  
+Aws.config.update({
+  region: ENV["AWS_REGION"],
+  credentials: Aws::Credentials.new(ENV["AWS_ACCESS_KEY_ID"], ENV["AWS_SECRET_ACCESS_KEY"])
+})
+
+s3 = Aws::S3::Resource.new
+bucket = s3.bucket(ENV["AWS_BUCKET"])
+
+Refile.cache = Refile::Backend::S3.new(prefix: "cache", bucket: bucket)
+Refile.store = Refile::Backend::S3.new(prefix: "store", bucket: bucket)
